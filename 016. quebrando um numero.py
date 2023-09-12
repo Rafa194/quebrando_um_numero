d67b2833dd17861f30aef521f3269bb63b8ba605
Rafael Prado <rafaelnascimentoprado@hotmail.com>
2023-09-12 11:46:21 -0300

    first commit

---
+++ 016. quebrando um numero.py
@@ -0,0 +1,18 @@
+num = float(input('Digite um número: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'
+      .format(num, int(num)))
+
+
+
+
+
+
+
+
+
+
+
+
+# num = float(input('Digite um número: '))
+# print('O valor digitado foi {} e a sua porção inteira é {}'
+#        .format(num, round(num + 0.5)))
